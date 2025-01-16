74fea2eb92cec120f2a03635fa1f1b5764525b57
Owen Hong <owenhong04@gmail.com>
2025-01-16 14:00:01 -0500

    Fixed the dependency tree issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,5 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
 
 
 /** @type {import('eslint').Linter.Config[]} */
